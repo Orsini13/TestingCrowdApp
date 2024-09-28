9312979c80ef6b65106b6a19b0ee5a83cb13294a
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-09-28 23:52:57 +0100

    first test

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import './index.css'
+import { Home, Profile, CreateCampaigns, CampaignDetails,FundCard } from './Pages' ;
+import {Sidebar , Navbar, } from './Components'
+
+const  App = () => {
+
+  return (
+   <div className='relative sm:-8 p-4 bg-[#ebf3bd] min-h-screen flex flex-row'>
+      {/* <div className='sm:flex hidden mr-10 relative'>
+        <Sidebar/>
+      </div> */}
+
+
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+          <Navbar />
+          
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/create-campaign' element={<CreateCampaigns  />}/>
+            <Route path='/Campaing-details/:id' element={<CampaignDetails/>}/>
+            <Route path='/FundCard' element={<FundCard />}/>
+          </Routes>
+
+      </div>
+   </div>
+
+  )
+
+}
+
+export default App
---
+++ src/Pages/index.jsx
@@ -0,0 +1,6 @@
+export {default as Home} from './Home.jsx';
+export {default as Profile} from './Profile.jsx';
+export {default as CreateCampaigns} from './CreateCampaigns.jsx';
+export {default as CampaignDetails} from './CampaignDetails.jsx';
+export {default as FundCard} from './FundCard';
+
