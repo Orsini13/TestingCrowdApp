9d411e920a2c1849da29a0efdee9fa8a151f9cf1
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-10-01 03:40:44 +0100

    ann butn

--- src/Components/DisplayCampaigns.jsx
+++ src/Components/DisplayCampaigns.jsx
@@ -18,12 +18,12 @@ const DisplayCampaigns = () => {
                 </div>
                 
                 <div className='flex gap-4'>
-                 <a  className=' flex py-[8px] px-[5px] rounded-sm mt-[6px] text-[#040f3dcc1] bg-[#afcee2]	'>
-                     <h1 className='font-serif'>Full details</h1> 
-                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <button id='btn' className='flex py-[8px] px-[5px] rounded-sm mt-[6px] text-[#040f3dcc1] bg-[#afcee2]'>
+                        See more
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                          <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
-                    </svg>
-                 </a>    
+                        </svg>
+                        </button>   
                 </div>
                 
 
